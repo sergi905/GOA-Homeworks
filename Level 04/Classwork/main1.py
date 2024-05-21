91846501badb9cea8d0ed0edd0b4a3d8c323982a
sergi905 <schikhradze9@gmail.com>
2024-05-22 01:33:20 +0400

    commit

---
+++ Level 04/Classwork/main1.py
@@ -0,0 +1,22 @@
+number = 1000
+# age = 15
+# balance = 500
+# print(5+5)
+# sum = (5*5)
+# print (str(sum) + "" + str(age))
+# print((5*5) + 5)
+# # udris 30 !!!!!!!!!!!!
+# age = 15
+# print(age)
+# age = 17
+# print(age)
+# age = 14
+# age = age + 1
+# print(age)
+# deklaracia (declaration) aris cvladis sheqmna
+# initializacia (initialize) cvladis mnishvnelobis minicheba
+age = 15
+age /= 2
+age *= 2
+age += 2
+age -= 2
