77e5400d5a32322ce68e0bbb870c656dc15856b0
sergi905 <schikhradze9@gmail.com>
2024-05-29 09:18:53 +0400

    commit

---
+++ Level 06/Classwork/main.py
@@ -0,0 +1,15 @@
+number_1 = int(input())
+number_2 = int(input())
+print (number_1 + number_2)
+print (number_1 - number_2)
+print (number_1 * number_2)
+print (number_1 / number_2)
+
+print (number_1 <= number_2)
+print (number_1 >= number_2)
+
+
+name = print((input("please input your name: ")))
+last_name = print(input("please input your last name: "))
+age = print(int(input("please input your age: ")))
+email = print(input("please input your E-Mail: "))
