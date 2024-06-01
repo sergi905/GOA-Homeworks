d89ba823e62f41aa6122f2af15244ff120457172
sergi905 <schikhradze9@gmail.com>
2024-06-01 21:01:48 +0400

    commit

--- Level 06/Homework/hw.py
+++ Level 06/Homework/hw.py
@@ -1,33 +1,33 @@
-# # PIRVELI DAVALEBA ((((((((((((((((((((((((()))))))))))))))))))))))))
+# PIRVELI DAVALEBA ((((((((((((((((((((((((()))))))))))))))))))))))))
 
-# # inputting numbers
-# number_1 = int(input("please input one number: "))
-# number_2 = int(input("please input second number: "))
+# inputting numbers
+number_1 = int(input("please input one number: "))
+number_2 = int(input("please input second number: "))
 
-# # summing numbers
-# print(number_1 + number_2)
-# print(number_1 - number_2)
-# print(number_1 * number_2)
-# print(number_1 / number_2)
+# summing numbers
+print(number_1 + number_2)
+print(number_1 - number_2)
+print(number_1 * number_2)
+print(number_1 / number_2)
 
-# # # MEORE DAVALEBA (((((((((((((((((((((((((())))))))))))))))))))))))))
+# # MEORE DAVALEBA (((((((((((((((((((((((((())))))))))))))))))))))))))
 
-# # inputting info
+# inputting info
 
-# name = str(input("please input your name: "))
-# last_name = str(input("please input your last name: "))
-# print("Welcome, " + name + " "+ last_name + "!")
+name = str(input("please input your name: "))
+last_name = str(input("please input your last name: "))
+print("Welcome, " + name + " "+ last_name + "!")
 
 
-# # MESAME DAVALEBA ((((((((((((((((((((((((((()))))))))))))))))))))))))))
+# MESAME DAVALEBA ((((((((((((((((((((((((((()))))))))))))))))))))))))))
 
-# name1 = input("Please input your name: ")
-# last_name1 = input("please input your last name: ")
-# age = input("Please input your age: ")
+name1 = input("Please input your name: ")
+last_name1 = input("please input your last name: ")
+age = input("Please input your age: ")
 
-# # outputting the info
+# outputting the info
 
-# print(name1 +", " +  last_name1 + ", " + age + " " + "Years old")
+print(name1 +", " +  last_name1 + ", " + age + " " + "Years old")
 
 
 # # MEOTXE DAVALEBA ((((((((((((((((((((((((((()))))))))))))))))))))))))))
