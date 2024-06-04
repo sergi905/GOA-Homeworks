80c0e3130c2ebb1326dfe5477cdcf3065b071b38
sergi905 <schikhradze9@gmail.com>
2024-06-04 21:12:11 +0400

    commit

---
+++ Level 08/Classwork/main.py
@@ -0,0 +1,65 @@
+
+# num2 = float(input("please input your digited number: "))
+# num3 = int(input("please input your second number: "))
+
+
+# print(num2 + num3)
+
+# num1 = 2
+# print(float(num1))
+
+
+
+# num = 10
+# print("my age: " + str(10))
+
+# print(5 > 5)
+# print(10.0 == 10)
+
+# print(str(false))
+
+
+# Pass = input("input your password: ")
+# passRepeat = input("input your password again: ")
+# print( Pass == passRepeat)
+
+
+
+# numbre1 = int(input("type number1: "))
+# numbre2 = int(input("type number2: "))
+
+# print(numbre1 > numbre2)
+# print(numbre1 < numbre2)
+# print(numbre1 >= numbre2)
+# print(numbre1 <= numbre2)
+# print(numbre1 > 1 and numbre1<10)
+
+
+
+
+# >5    <=10
+
+cifri = int(input("input a number between 6 and 10: "))
+
+print(cifri > 5 and cifri <= 10)
+
+
+# >5  >10
+
+cifri1 = int(input("input a number more than 5 and 10: "))
+
+print(cifri1 > 5 or cifri > 10)
+
+
+# comps
+
+print(True and False)
+print(True and True)
+print(False and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+
+
+
---
+++ Level 08/Homework/5).py
@@ -0,0 +1,8 @@
+numbre1 = int(input("type number1: "))
+numbre2 = int(input("type number2: "))
+
+print(numbre1 > numbre2)
+print(numbre1 < numbre2)
+print(numbre1 >= numbre2)
+print(numbre1 <= numbre2)
+print(numbre1 > 1 and numbre1<10)
