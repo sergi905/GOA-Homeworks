3456d2d24529466cab25e8f6d0eaa96656c470b0
sergi905 <schikhradze9@gmail.com>
2024-06-21 11:42:13 +0400

    commit

---
+++ Level 12/Classwork/py.py
@@ -0,0 +1,31 @@
+# age = 6
+
+# while age > 0:
+#     print("hello")
+#     age= age - 1
+
+
+# age = 0
+
+# while age < 6:
+#     print("hello")
+#     age = age + 1
+
+# for i in range (3):
+#     print("hw")
+#     print("rw")
+
+# age = int(input("input your age please: "))
+# if age >= 18 and age > 0:
+#     print("you are an adult")    
+# elif age <0:
+#     print("you are not born.....yet")    
+# else:
+#     print("you are a kid")
+
+
+age = 20
+if age >= 18:
+    print("regular")
+else:
+    print("discount")    
