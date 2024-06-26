a6477d373411a8639e059d24567e9109c5a86fe0
sergi905 <schikhradze9@gmail.com>
2024-06-26 16:33:20 +0400

    commit

---
+++ Level 14/Classwork/cw.py
@@ -0,0 +1,79 @@
+# print("luka")
+# axe = 2
+# print("like")
+# while axe > 2:
+#     print("robux")
+# else:
+#     print("bob")     
+
+# for i in range(2,10, 3):
+#     print("luka")
+      
+
+# #     #________________________________ Classwork   
+  
+# for i in range (0, 10+1 , 2):
+#     print(i) 
+
+
+# ________________________________
+
+# for i in range(1, 11, 2):
+#     print (i)
+
+
+# for i in range (0, 101, 5):
+#     print(i)    
+
+
+
+# for i in range (0, 101, 3):
+#     print(i)  
+
+# for i in range (20, -1, -1):
+#     print(i)    
+
+
+
+# num1 = int(input("number1: "))     
+# num2 = int(input("number2: "))  
+
+# for i in range (num1, num2+1):
+#     print(i)
+
+# sum = 0
+
+# for i in range (1, 11,):
+#     sum = sum + i 
+
+
+
+
+# THIS IS IMPORTANT!!!!!!!!!!!!!!!!!!!
+#  sum = 0
+
+# for i in range(0, 10 + 1, 2):
+#     sum = sum + i
+
+# print(sum)   
+
+# num = 0
+# while num <=10:
+#     num = num +1
+#     print(num)
+
+counter = 0
+
+# paroli aris 1234
+password = 1234
+
+# vadzlev variables momxmareblis mier chaweril parols
+inputt = int((input("input the password: 1234: ")))
+
+# tu mititebuli ar udris 1234-s, mashin igive gamouchinos momxmarebels
+while inputt != password:
+    (input("input the password: 1234: "))
+    counter = counter + 1
+# print("it took you"   + str(counter) tries to get the password)
+if inputt == password:
+    (print("it took you"   + str(counter) +  "tries to get the password"))
