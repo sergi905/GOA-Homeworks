e2be85eac61ac2858ccd428b79f7cd12c1b8ab6a
sergi905 <schikhradze9@gmail.com>
2024-07-06 12:53:14 +0400

    commit

---
+++ Level 16/Classwork/dw.py
@@ -0,0 +1,31 @@
+# names = ["nika", "luka", "sandro", "gio", "data"]
+
+# print(names)
+# names.append("xdd")
+# print(names)
+
+# names.pop(0)
+# print(names)
+
+
+cars = ["bmw", "lambo","suzuki"]
+# for i in range(5):
+#     next = input("input a car name: ")
+#     cars.append(next)       
+# print(cars)
+# print(len(cars))
+# print(cars.index("bmw"))
+# print(cars.count("bmw"))
+
+
+# 1.)
+
+print(cars.index("lambo"))
+
+# 2.)
+
+print(cars.count("bmw"))
+
+# 3.)
+print(len(cars))
+
