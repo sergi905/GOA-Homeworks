d93cc6f7d4bc139b56fe53bd113cab33d04c0672
sergi905 <schikhradze9@gmail.com>
2024-07-27 16:08:04 +0400

    commit

---
+++ Level 20/Classwork/function.py
@@ -0,0 +1,57 @@
+
+# name = input("input your name: ")
+# def greeting():
+#     print("hello, " + name)
+
+
+
+
+
+# def add(num1, num2, operation):
+#     print(num1 + num2)
+
+# add(5,3)
+# add(7,21)        
+
+
+
+def function(num1,num2, operator):
+    print(num1 + num2)
+
+    if operator == "+":
+        refresh = (num1 + num2)
+    elif operator == "-":
+        refresh = (num1 - num2)  
+    elif operator == "/":
+        refresh = (num1/num2)    
+    elif operator == "*":
+        refresh = (num1*num2)    
+    return refresh    
+
+
+function(2,3, "+")
+function(4,6, "-")
+function(10,22, "/")
+function(1,6, "*")      
+
+
+
+
+def numbers(int1, int2):
+    print(int1+int2)
+
+
+numbers(4,2)
+
+if numbers == 
+
+
+
+
+
+
+
+
+
+
+    
