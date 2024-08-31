0c4ee1b4ab1e732bc48d6b8515a39b256d23666f
sergi905 <schikhradze9@gmail.com>
2024-08-31 10:27:54 +0400

    commit

---
+++ Level 24/Classwork/functions.py
@@ -0,0 +1,83 @@
+# def manual_sort(int_list):
+#     for x in range(i):
+#         for i in range(len(int_list) - 1 ):
+#             if int_list[i] > int_list[i + 1]:
+#                 bigger = int_list[x]
+#                 smaller = int_list[i]
+#                 int_list[x] = smaller
+       
+#     return int_list            
+
+
+# print(manual_sort([4,2,10,5,0]))
+
+# 
+
+# def leave_evens(number_list):
+#    res_list = []
+   
+#    for num in number_list:
+#         if num %2 == 0:
+#             res_list.append(num)
+#         print(num)    
+
+#     return res_list            
+
+
+
+# print(even_list[1,2,3,4,5,6,7,8,9])
+
+
+
+def string_func(string_list):
+    res_list = []
+
+    for string in string_list:
+        if len(string) <=4:
+            res_list.append(string)
+
+
+
+
+    return res_list
+
+
+print(string_func(["hello", "goa", "yuh", "123", "david"]))
+
+def prime_check(user_num):
+    is_prime = True
+
+    for i in range(2, user_num):
+        if user_num%i == 0:
+            is_prime == False
+
+
+
+    return is_prime
+
+print(prime_check(10))
+
+
+
+
+
+
+["aaaa", "aa" "a"]
+
+def manual_string_sort(string_list):
+    for i in range(len(string_list)):
+        for x in range(i):
+            if string_list[x] >
+
+
+
+
+def all_numbers(numbers)
+    for i in numbers:
+        if type(all_numbers[i])
+
+    
+
+
+
+all_numbers([1, 2 , 2.2 , 3 , 4 , 4.4 , 5])
---
+++ Level 26/Classwork/functions.py
@@ -0,0 +1,50 @@
+name = "GIORGI"
+print(name)
+
+if name == "giorgi":
+    print(True)
+
+name = name.upper()
+print(name)
+
+
+food = ["kinkali", "xachapuri" , "sazamtro"]
+
+food.index(1)
+
+
+
+name = "giorgi" 
+print(name[1:5])
+
+
+
+# es poulobs micemul asos an asoebis kombinacias
+name = "abcdef"
+print(name.find("a"))
+
+city = "bern"
+
+print(city.capitalize())
+
+
+
+
+ting = "vehicle"
+print(ting.find(e))
+
+
+names = ["nika" 'luka' "gio"]
+print(len.names)
+
+
+def sum(num1, num2):
+    print(num1 + num2)
+
+
+print(sum(5, 5))
+
+
+
+sum = []
+
